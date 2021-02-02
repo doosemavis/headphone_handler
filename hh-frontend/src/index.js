63132f6b395b67b8a97dac6319d8aea813f18c6a
doosemavis <moosedavis2011@gmail.com>
2021-02-02 14:04:07 -0600

    react/redux set-up & wired

--- hh-frontend/src/index.js
+++ hh-frontend/src/index.js
@@ -7,9 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index.js'
-import { composeWithDevTools } from 'redux-devtools-extention'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = (rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
