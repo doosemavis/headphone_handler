f7b2bf760275371421f764be72fba5e6463cf10b
doosemavis <moosedavis2011@gmail.com>
2021-02-02 14:24:53 -0600

    import { connect } from 'react-redux'

--- hh-frontend/src/App.js
+++ hh-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
@@ -12,4 +12,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
