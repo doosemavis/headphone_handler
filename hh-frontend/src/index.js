87e70d1ebd8203018123c544571b394f1f090f76
doosemavis <moosedavis2011@gmail.com>
2021-02-02 13:58:51 -0600

    add devtools and applyMiddleware to src/index.js

--- hh-frontend/src/index.js
+++ hh-frontend/src/index.js
@@ -7,6 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index.js'
+import { composeWithDevTools } from 'redux-devtools-extention'
+
+const store = (rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
