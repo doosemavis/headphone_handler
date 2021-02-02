b18bd6d3b02bccab030372291d0411b5e38cd0f6
doosemavis <moosedavis2011@gmail.com>
2021-02-02 14:42:44 -0600

    adding info to App.js

--- hh-frontend/src/App.js
+++ hh-frontend/src/App.js
@@ -4,14 +4,25 @@ import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    const brandsLis = this.props.brands.map((br) => <li key={br.id}>{br.name}</li>)
+
     return (
       <div className="App">
-        <h2>Our Brands</h2>
+        <h2>Headphone Brands</h2>
         <hr/>
         <h2>Create Brands</h2>
+        {this.props.loading ? <h3>Loading. . .</h3> : brandsLis}
       </div>
     );
   }
 }
 
-export default connect()(App);
+const mapStateToProps = (state) => {
+  console.log("I am state", state)
+  return {
+    brands: state.brandReducer.brands,
+    loading: state.brandReducer.loading,
+  }
+}
+
+export default connect(mapStateToProps)(App);
