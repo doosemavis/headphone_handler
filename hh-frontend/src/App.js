f4f8cc667ae05574ef08af36a18c6113aded6cf4
doosemavis <moosedavis2011@gmail.com>
2021-02-03 11:35:47 -0600

    fixed routing issues

--- hh-frontend/src/App.js
+++ hh-frontend/src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { getBrands } from './actions/brands.js';
-import BrandForm from './containers/BrandForm.js';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import BrandContainer from './containers/BrandContainer.js';
 
 class App extends Component {
-  
-  componentDidMount() {
-    this.props.getBrands()
-  }
 
   render() {
     console.log("Rendering")
-    const brandsLis = this.props.brands.map((br) => <li key={br.id}>{br.name}</li>)
 
     return (
       <Router>
-        <div className="App">
-          <h2>Create Brands</h2>
-          <BrandForm/>
-          <hr/>
-          <h2>Headphone Brands</h2>
-            <ol>
-                {this.props.loading ? <h3>Loading. . .</h3> : brandsLis}
-            </ol>
-        </div>
-        <Route exact path="/" />
+        <Route exact path="/" component={BrandContainer}/>
         <Route exact path="/brands" />
         <Route exact path="/headphones" />
       </Router>
@@ -34,12 +18,5 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  console.log("I am state", state)
-  return {
-    brands: state.brandReducer.brands,
-    loading: state.brandReducer.loading,
-  }
-}
 
-export default connect(mapStateToProps, { getBrands })(App);
+export default App;
---
+++ hh-frontend/src/containers/BrandContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import BrandForm from './BrandForm.js';
+import { connect } from 'react-redux';
+import { getBrands } from '../actions/brands.js';
+
+class BrandContainer extends Component {
+    componentDidMount() {
+        this.props.getBrands()
+    }
+    
+    render() {
+        console.log("Rendering")
+        const brandsOls = this.props.brands.map((br) => <li key={br.id}>{br.name}</li>)
+    
+        return (
+            <div className="App">
+            <h2>Create Brands</h2>
+            <BrandForm/>
+            <hr/>
+            <h2>Headphone Brands</h2>
+                <ol>
+                    {this.props.loading ? <h3>Loading. . .</h3> : brandsOls}
+                </ol>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+  console.log("I am state", state)
+  return {
+    brands: state.brandReducer.brands,
+    loading: state.brandReducer.loading,
+  }
+}
+
+export default connect(mapStateToProps, {getBrands})(BrandContainer);
