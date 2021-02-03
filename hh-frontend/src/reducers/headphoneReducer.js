bc0106f4ba7bb685cac01ab0cc949cc00fa11e3b
doosemavis <moosedavis2011@gmail.com>
2021-02-03 12:34:42 -0600

    added actions to headphoneReducer.js

--- hh-frontend/src/reducers/headphoneReducer.js
+++ hh-frontend/src/reducers/headphoneReducer.js
@@ -1,8 +1,34 @@
 const headphoneReducer = (state = {headphones: [], loading: false}, action) => {
     switch(action.type) {
+        case "LOADING_HEADPHONES":
+            return {
+                ...state,
+                loading: true
+            }
+
+        case "FETCH_HEADPHONES":
+            return {
+                ...state,
+                headphones: action.payload,
+                loading: false
+            }    
+
+        case "ADD_HEADPHONE":
+            return {
+                ...state,
+                loading: true
+            } 
+        
+        case "BRAND_ADDED":
+            return {
+                ...state,
+                headphones: [...state.headphones, action.payload],
+                loading: false
+            }
+
         default:
             return state;
-    }
+    } 
 }
 
 export default headphoneReducer;
