3f3a1b44219f98be40313b7a9a86d4ae70d1ffa1
doosemavis <moosedavis2011@gmail.com>
2021-02-03 12:30:22 -0600

    added fetchHeadphone & addHeadphone to action/headphone.js

--- hh-frontend/src/actions/headphones.js
+++ hh-frontend/src/actions/headphones.js
@@ -1,5 +1,23 @@
 export const getHeadphones = () => {
-    return (dispatch) = {
+    return (dispatch) => {
+        dispatch({type: "LOADING_HEADPHONES"})
+        fetch('/headphones')
+        .then(res => res.json())
+        .then(headphones => dispatch({type: "FETCH_HEADPHONES", payload: headphones}))
+    }
+}
 
+export const addHeadphone = (headphone) => {
+    return (dispatch) => {
+        dispatch({type: "ADD_BRAND"})
+        fetch('/headphones', {
+            method: 'POST',
+            body: JSON.stringify(headphone),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(headphone => dispatch({type: "HEADPHONE_ADDED"}))
     }
 }
