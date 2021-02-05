c7ffbda5e26f2fa819d51dd3f7fb2182314c0a09
doosemavis <moosedavis2011@gmail.com>
2021-02-05 13:44:29 -0600

    tada

--- hh-frontend/src/components/Home.js
+++ hh-frontend/src/components/Home.js
@@ -5,7 +5,10 @@ class Home extends Component {
     render() {
         return (
             <div>
+                <br/>
+                <br/>
                 <h1>Welcome to the Headphone Handler</h1>
+                <hr/>
                 <h4>Please select a link from the navagation bar above</h4>
             </div>
         )
